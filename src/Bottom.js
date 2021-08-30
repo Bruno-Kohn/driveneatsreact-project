55b16f1a6dd7aa077be107cabb91fcae231cd93f
Bruno Kohn <brunokohn1993@gmail.com>
2021-08-30 13:53:16 -0300

    enviando mensagem

--- src/Bottom.js
+++ src/Bottom.js
@@ -3,6 +3,8 @@ export default function Bottom({ pedido, setPedido }) {
   let numPratos = 0;
   let numBebidas = 0;
   let numSobremesa = 0;
+  let message = "";
+  let totalPrice = 0;
 
   const pedidoFiltrado = pedido.filter((p, i) => pedido.indexOf(p) === i);
   console.log(pedidoFiltrado, "FILTRADO");
@@ -37,6 +39,46 @@ export default function Bottom({ pedido, setPedido }) {
     }
   }
 
+  function sendMessage() {
+    for (let i = 0; i < pedidoFiltrado.length; i++) {
+      if (
+        pedidoFiltrado[i].classeOpcao === "cbo" ||
+        pedidoFiltrado[i].classeOpcao === "bigmac" ||
+        pedidoFiltrado[i].classeOpcao === "mcchicken" ||
+        (pedidoFiltrado[i].classeOpcao === "cheddar" &&
+          pedidoFiltrado[i].qtd > 0)
+      ) {
+        message += `Prato: ${pedidoFiltrado[i].opcao} (${pedidoFiltrado[i].qtd}x)\n`;
+        totalPrice += pedidoFiltrado[i].preco * pedidoFiltrado[i].qtd;
+      }
+      if (
+        pedidoFiltrado[i].classeOpcao === "coca" ||
+        pedidoFiltrado[i].classeOpcao === "fanta" ||
+        pedidoFiltrado[i].classeOpcao === "guarana" ||
+        (pedidoFiltrado[i].classeOpcao === "sprite" &&
+          pedidoFiltrado[i].qtd > 0)
+      ) {
+        message += `Bebida: ${pedidoFiltrado[i].opcao} (${pedidoFiltrado[i].qtd}x)\n`;
+        totalPrice += pedidoFiltrado[i].preco * pedidoFiltrado[i].qtd;
+      }
+      if (
+        pedidoFiltrado[i].classeOpcao === "sundae" ||
+        pedidoFiltrado[i].classeOpcao === "casquinha" ||
+        pedidoFiltrado[i].classeOpcao === "mcflurry" ||
+        (pedidoFiltrado[i].classeOpcao === "torta" && pedidoFiltrado[i].qtd > 0)
+      ) {
+        message += `Sobremesa: ${pedidoFiltrado[i].opcao} (${pedidoFiltrado[i].qtd}x)\n`;
+        totalPrice += pedidoFiltrado[i].preco * pedidoFiltrado[i].qtd;
+      }
+      
+    }
+    message += `Total: R$${totalPrice.toFixed(2)}`
+    console.log(message);
+    const encodedMessage = encodeURIComponent(message);
+
+    window.open(`https://wa.me/5521981037125?text=${encodedMessage}`);
+  }
+
   return (
     <div className="bottom">
       <button
@@ -44,9 +86,7 @@ export default function Bottom({ pedido, setPedido }) {
           numPratos > 0 && numBebidas > 0 && numSobremesa > 0 ? "check" : ""
         }
         onClick={
-          numPratos > 0 && numBebidas > 0 && numSobremesa > 0
-            ? () => alert()
-            : ""
+          numPratos > 0 && numBebidas > 0 && numSobremesa > 0 ? sendMessage : ""
         }
       >
         {numPratos > 0 && numBebidas > 0 && numSobremesa > 0
